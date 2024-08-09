bf20179480741b2a7bc2c253d4b8118051bacd6c
tran van tien <trantien03@gmail.com>
2024-08-09 08:57:41 +0700

    Initial commit

---
+++ ApplicationExample.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:path/path.dart' as p;
+
+// class Score {
+//   int score;
+//
+//   Score(this.score);
+// }
+//
+// class Subject {
+//   String name;
+//   List<Score> scores;
+//
+//   Subject(this.name, this.scores);
+// }
+//
+// class Student {
+//   int id;
+//   String name;
+//   List<Subject> subjects;
+//
+//   Student(this.id, this.name, this.subjects);
+// }
